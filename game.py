f5714acd0bb60fa7cdcafb878867efd8e5debe58
Rômulo Souza <romulovinicius10@gmail.com>
2019-06-06 00:28:34 -0300

    Added method to print numbers in matrix

--- game.py
+++ game.py
@@ -21,41 +21,58 @@ clock = pygame.time.Clock()
 screen = pygame.display.set_mode((screen_width, screen_height))
 
 # pygame.mouse.set_visible(0)
-
+BLOCK_SIZE = 30
+MATRIX_SPACE = 30
 background = pygame.image.load('images/space.jpg')
 
-def draw_matrix_a(size, matrix):
-    block_size = 20
-    matrix_space = 30
-    if matrix == 'A':
-        initial_pos_x = (screen_width/2)-((block_size+1)*size)-matrix_space
-        initial_pos_y = (screen_height/2)-(block_size*size)-matrix_space
-    elif matrix == 'B':
-        initial_pos_x = (screen_width/2)+matrix_space  
-        initial_pos_y = (screen_height/2)-(block_size*size)-matrix_space
-    elif matrix == 'C':
-        initial_pos_x = (screen_width/2)-((block_size+1)*size/2)  
-        initial_pos_y = (screen_height/2)+matrix_space
-  
+def text_objects(text, font):
+    textSurface = font.render(text, True, white)
+    width = textSurface.get_width()
+    height = textSurface.get_height()
+    return textSurface, textSurface.get_rect(), width, height
+
+def draw_matrix_number(number, pos_x, pos_y):
+    text = pygame.font.Font('freesansbold.ttf', 13)
+    text_surface = text.render(str(number), True, white)
+    text_surface_rect = text_surface.get_rect()
+    text_surface_rect.center = (pos_x+BLOCK_SIZE/2, pos_y+BLOCK_SIZE/2)
+    screen.blit(text_surface, text_surface_rect)
+
+def draw_matrix_grid(size, matrix_id, matrix=None):
+    if matrix_id == 'A':
+        initial_pos_x = (screen_width/2)-((BLOCK_SIZE+1)*size)-MATRIX_SPACE
+        initial_pos_y = (screen_height/2)-(BLOCK_SIZE*size)-MATRIX_SPACE
+    elif matrix_id == 'B':
+        initial_pos_x = (screen_width/2)+MATRIX_SPACE  
+        initial_pos_y = (screen_height/2)-(BLOCK_SIZE*size)-MATRIX_SPACE
+    elif matrix_id == 'C':
+        initial_pos_x = (screen_width/2)-((BLOCK_SIZE+1)*size/2)  
+        initial_pos_y = (screen_height/2)+MATRIX_SPACE
+
+
     for y in range(size):
         for x in range(size):
-            pos_x = x*(block_size+1) + initial_pos_x
-            pos_y = y*(block_size+1) + initial_pos_y
-            rect = pygame.Rect(pos_x, pos_y , block_size, block_size)
+            pos_x = x*(BLOCK_SIZE+1) + initial_pos_x
+            pos_y = y*(BLOCK_SIZE+1) + initial_pos_y
+            rect = pygame.Rect(pos_x, pos_y , BLOCK_SIZE, BLOCK_SIZE)
             pygame.draw.rect(screen, (255, 0, 0), rect)
+            if matrix is not None:
+                draw_matrix_number(matrix[x][y], pos_x, pos_y)
+
 
 class Game:
     def __init__(self):
+        self.size = 10
         pass
 
     def run(self):
-        size = 10
+        matrix = [[1 for x in range(self.size)] for y in range(self.size)]
         while True:
             screen.blit(background, [0, 0])
             # pygame.draw.rect(screen, (255, 0, 0), pygame.Rect(500, 0, 40, 30))
-            draw_matrix_a(size, 'A')
-            draw_matrix_a(size, 'B')
-            draw_matrix_a(size, 'C')
+            draw_matrix_grid(self.size, 'A', matrix)
+            draw_matrix_grid(self.size, 'B', matrix)
+            draw_matrix_grid(self.size, 'C')
             # draw_matrix_b(size)
 
             for event in pygame.event.get():
